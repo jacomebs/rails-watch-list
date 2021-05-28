cd61c732164698c54ba98572c0c1406375537199
Jacome Saavedra <jacomebsaavedra@gmail.com>
2021-05-28 14:29:01 +0100

    Updating the seeds for 5 times the size

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,19 @@ puts 'Deleting everything...'
 Bookmark.destroy_all
 Movie.destroy_all
 
-puts 'Creating movies...'
-url = 'http://tmdb.lewagon.com/movie/top_rated'
+5.times do |i|
+  puts "Importing movies from page #{i + 1}..."
 
-movies_json = JSON.parse(RestClient.get(url).body, symbolize_names: true)
+  movies_json = JSON.parse(RestClient.get("http://tmdb.lewagon.com/movie/top_rated?page=#{i + 1}").body, symbolize_names: true)
 
-movies_json[:results].each do |movie_json|
-  Movie.create!(
-    title: movie_json[:title],
-    overview: movie_json[:overview],
-    poster_url: "https://image.tmdb.org/t/p/w500/#{movie_json[:poster_path]}",
-    rating: movie_json[:vote_average]
-  )
+  movies_json[:results].each do |movie_json|
+    Movie.create!(
+      title: movie_json[:title],
+      overview: movie_json[:overview],
+      poster_url: "https://image.tmdb.org/t/p/w500/#{movie_json[:poster_path]}",
+      rating: movie_json[:vote_average]
+    )
+  end
 end
 
 puts 'Done 🎉'
